188c31207fbaf760dcd755cfea261939102b76d2
ALinuxPerson <alinuxperson@gmail.com>
2023-06-05 12:40:04 +0800

    Move macros to its own module

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,65 @@
+#[macro_use]
+mod macros {
+    #[allow(non_snake_case)]
+    macro_rules! impl_ReadFrom_for {
+        ($ty:ty) => {
+            #[async_trait]
+            impl ReadFrom for $ty {
+                async fn readable(&self) -> io::Result<()> {
+                    <$ty>::readable(self).await
+                }
+
+                fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+                    <$ty>::try_read(self, buf)
+                }
+            }
+        };
+
+        (lt $ty:ident) => {
+            #[async_trait]
+            impl<'a> ReadFrom for $ty<'a> {
+                async fn readable(&self) -> io::Result<()> {
+                    <$ty>::readable(self).await
+                }
+
+                fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+                    <$ty>::try_read(self, buf)
+                }
+            }
+        };
+    }
+
+    #[allow(non_snake_case)]
+    macro_rules! impl_WriteTo_for {
+        ($ty:ty) => {
+            #[async_trait]
+            impl WriteTo for $ty {
+                async fn writable(&self) -> io::Result<()> {
+                    <$ty>::writable(self).await
+                }
+
+                fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
+                    <$ty>::try_write(self, buf)
+                }
+            }
+        };
+
+        (lt $ty:ident) => {
+            #[async_trait]
+            impl<'a> WriteTo for $ty<'a> {
+                async fn writable(&self) -> io::Result<()> {
+                    <$ty>::writable(self).await
+                }
+
+                fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
+                    <$ty>::try_write(self, buf)
+                }
+            }
+        };
+    }
+
+}
+
 pub mod dirs;
 pub mod serve;
 pub mod utils;
@@ -73,35 +135,6 @@ pub trait ReadFrom: Sync {
     }
 }
 
-#[allow(non_snake_case)]
-macro_rules! impl_ReadFrom_for {
-    ($ty:ty) => {
-        #[async_trait]
-        impl ReadFrom for $ty {
-            async fn readable(&self) -> io::Result<()> {
-                <$ty>::readable(self).await
-            }
-
-            fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
-                <$ty>::try_read(self, buf)
-            }
-        }
-    };
-
-    (lt $ty:ident) => {
-        #[async_trait]
-        impl<'a> ReadFrom for $ty<'a> {
-            async fn readable(&self) -> io::Result<()> {
-                <$ty>::readable(self).await
-            }
-
-            fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
-                <$ty>::try_read(self, buf)
-            }
-        }
-    };
-}
-
 impl_ReadFrom_for!(OwnedTcpReadHalf);
 impl_ReadFrom_for!(OwnedUnixReadHalf);
 impl_ReadFrom_for!(lt TcpReadHalf);
@@ -151,34 +184,6 @@ pub trait WriteTo: Sync {
     }
 }
 
-#[allow(non_snake_case)]
-macro_rules! impl_WriteTo_for {
-    ($ty:ty) => {
-        #[async_trait]
-        impl WriteTo for $ty {
-            async fn writable(&self) -> io::Result<()> {
-                <$ty>::writable(self).await
-            }
-
-            fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
-                <$ty>::try_write(self, buf)
-            }
-        }
-    };
-
-    (lt $ty:ident) => {
-        #[async_trait]
-        impl<'a> WriteTo for $ty<'a> {
-            async fn writable(&self) -> io::Result<()> {
-                <$ty>::writable(self).await
-            }
-
-            fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
-                <$ty>::try_write(self, buf)
-            }
-        }
-    };
-}
 
 impl_WriteTo_for!(OwnedTcpWriteHalf);
 impl_WriteTo_for!(OwnedUnixWriteHalf);
