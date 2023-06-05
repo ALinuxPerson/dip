6e067a3c86ba443acb3090092d25b42fd3e71453
ALinuxPerson <alinuxperson@gmail.com>
2023-06-05 08:53:08 +0800

    Extract inline module `dirs`

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,15 +1,5 @@
 mod utils;
-mod dirs {
-    use once_cell::sync::Lazy;
-    use std::path::{Path, PathBuf};
-
-    static HOST_TOML: Lazy<PathBuf> =
-        Lazy::new(|| dip_common::dirs().config_dir().join("host.toml"));
-
-    pub fn host_toml() -> &'static Path {
-        &HOST_TOML
-    }
-}
+mod dirs;
 
 use crate::utils::MaybeSocketAddr;
 use anyhow::Context;
