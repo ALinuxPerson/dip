020d6801905bd801b6a729fcbe3ff8c906938102
ALinuxPerson <alinuxperson@gmail.com>
2023-06-05 11:55:59 +0800

    Use lowercase "dip" on Linux, and fully uppercase "DIP" on non-Linux OS

--- common/src/dirs.rs
+++ common/src/dirs.rs
@@ -5,8 +5,14 @@ use std::sync::OnceLock;
 static DIRS: OnceLock<ProjectDirs> = OnceLock::new();
 
 pub fn initialize() -> anyhow::Result<()> {
+    #[cfg(target_os = "linux")]
+    let application = "dip";
+
+    #[cfg(not(target_os = "linux"))]
+    let application = "DIP";
+
     DIRS.set(
-        ProjectDirs::from("", "ALinuxPerson", "dip")
+        ProjectDirs::from("", "ALinuxPerson", application)
             .context("could not find project directories")?,
     )
     .unwrap_or_else(|_| panic!("`DIRS` already initialized"));
