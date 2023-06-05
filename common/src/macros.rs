5a6df4f51fccfcaececae5558b55d1182c11ad71
ALinuxPerson <alinuxperson@gmail.com>
2023-06-05 13:03:17 +0800

    Canonicalize macro paths

--- common/src/macros.rs
+++ common/src/macros.rs
@@ -1,26 +1,26 @@
 #[allow(non_snake_case)]
 macro_rules! impl_ReadFrom_for {
     ($ty:ty) => {
-        #[async_trait]
-        impl ReadFrom for $ty {
-            async fn readable(&self) -> io::Result<()> {
+        #[::async_trait::async_trait]
+        impl $crate::ReadFrom for $ty {
+            async fn readable(&self) -> ::std::io::Result<()> {
                 <$ty>::readable(self).await
             }
 
-            fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+            fn try_read(&self, buf: &mut [u8]) -> ::std::io::Result<usize> {
                 <$ty>::try_read(self, buf)
             }
         }
     };
 
     (lt $ty:ident) => {
-        #[async_trait]
-        impl<'a> ReadFrom for $ty<'a> {
-            async fn readable(&self) -> io::Result<()> {
+        #[::async_trait::async_trait]
+        impl<'a> $crate::ReadFrom for $ty<'a> {
+            async fn readable(&self) -> ::std::io::Result<()> {
                 <$ty>::readable(self).await
             }
 
-            fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+            fn try_read(&self, buf: &mut [u8]) -> ::std::io::Result<usize> {
                 <$ty>::try_read(self, buf)
             }
         }
@@ -30,26 +30,26 @@ macro_rules! impl_ReadFrom_for {
 #[allow(non_snake_case)]
 macro_rules! impl_WriteTo_for {
     ($ty:ty) => {
-        #[async_trait]
-        impl WriteTo for $ty {
-            async fn writable(&self) -> io::Result<()> {
+        #[::async_trait::async_trait]
+        impl $crate::WriteTo for $ty {
+            async fn writable(&self) -> ::std::io::Result<()> {
                 <$ty>::writable(self).await
             }
 
-            fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
+            fn try_write(&self, buf: &[u8]) -> ::std::io::Result<usize> {
                 <$ty>::try_write(self, buf)
             }
         }
     };
 
     (lt $ty:ident) => {
-        #[async_trait]
-        impl<'a> WriteTo for $ty<'a> {
-            async fn writable(&self) -> io::Result<()> {
+        #[::async_trait::async_trait]
+        impl<'a> $crate::WriteTo for $ty<'a> {
+            async fn writable(&self) -> ::std::io::Result<()> {
                 <$ty>::writable(self).await
             }
 
-            fn try_write(&self, buf: &[u8]) -> io::Result<usize> {
+            fn try_write(&self, buf: &[u8]) -> ::std::io::Result<usize> {
                 <$ty>::try_write(self, buf)
             }
         }
