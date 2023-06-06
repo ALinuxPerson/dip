e4221627d00cc8b958b62738fc32e5a57a6eae56
ALinuxPerson <alinuxperson@gmail.com>
2023-06-06 16:28:08 +0800

    Display config file location on debug logging only

--- remote/src/main.rs
+++ remote/src/main.rs
@@ -40,7 +40,7 @@ async fn try_main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     dip_common::dirs::initialize()?;
 
-    tracing::info!("config file location is {}", Config::toml().display());
+    tracing::debug!("config file location is {}", Config::toml().display());
 
     let span = tracing::info_span!("resolve config");
     let config = Config::read()?;
